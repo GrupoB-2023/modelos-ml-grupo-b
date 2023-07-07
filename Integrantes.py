4f238fbd2d1af032b4f378d45851acd7d951a271
Rafael-Cordova <rafaelcorsa97@gmail.com>
2023-07-06 21:29:55 -0500

    Subiendo despliegue 1

--- Integrantes.py
+++ Integrantes.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 st.set_page_config(
-    page_title="Tarea Semana 15",
+    page_title="Trabajo Grupal Semana 14",
 )
 
 st.write("# Despliegue web de modelos del Grupo B 2023-1")
@@ -11,15 +11,14 @@ st.sidebar.success("Seleccione un modelo del menu")
 st.markdown(
     """
     # Grupo B - Integrantes:
-    | Nombre |
     |--|--|
-    | Jimenez Castaneda, Luis Francisco - 15200213  |
-    | Del Aguila Febres Brayan - 17200270  |
-    | Cordova Sandoval Rafael - 17200268 |
-    | Caceres Estana Juan Alfonso - 19200288   |
-    | Ambrocio Milla Katherine Celine - 18200324  |
-    | Rios Sanchez Anthony Ulises - 19200099  |
-    | Hidalgo Diaz Sebastian Eduardo - 18200082  |
+    #Jimenez Castaneda, Luis Francisco - 15200213  
+    # Del Aguila Febres Brayan - 17200270  
+    # Cordova Sandoval Rafael - 17200268 
+    # Caceres Estana Juan Alfonso - 19200288   
+    # Ambrocio Milla Katherine Celine - 18200324  
+    # Rios Sanchez Anthony Ulises - 19200099  
+    # Hidalgo Diaz Sebastian Eduardo - 18200082  
 
     ### Especificaciones:
     **Donde muestra las predicciones/los resultados:**
