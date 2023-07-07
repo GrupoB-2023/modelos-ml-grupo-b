9e546685cc67aac38cad50f8ab9e58bbf0f875d0
Rafael-Cordova <rafaelcorsa97@gmail.com>
2023-07-06 21:13:57 -0500

    Despliegue con UTF 8

--- Hello.py
+++ Hello.py
@@ -14,10 +14,10 @@ st.markdown(
     # Grupo B - Integrantes:
     | Nombre |
     |--|--|
-    | Jimenez Casta?eda, Luis Francisco - 15200213  |
+    | Jimenez Castaneda, Luis Francisco - 15200213  |
     | Del Aguila Febres Brayan - 17200270  |
     | Cordova Sandoval Rafael - 17200268 |
-    | Caceres Esta?a Juan Alfonso - 19200288   |
+    | Caceres Estana Juan Alfonso - 19200288   |
     | Ambrocio Milla Katherine Celine - 18200324  |
     | Rios Sanchez Anthony Ulises - 19200099  |
     | Hidalgo D¨ªaz Sebastian Eduardo - 18200082  |
