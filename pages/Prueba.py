7b98215861cbacf6e3f252868f973e32f9034ec5
Juan Caceres <juanalfonso.caceres@unmsm.edu.pe>
2023-07-08 01:23:23 -0500

    Modi LSTM1

--- pages/Prueba.py
+++ pages/Prueba.py
@@ -1,8 +1,18 @@
 import streamlit as st
 
 # Título de la aplicación
-st.title("Envío de datos")
-
+st.title("MODELO LSTM CLASIFICADOR")
+# Descripción del modelo LSTM
+st.markdown("""
+            Un modelo LSTM (Long Short-Term Memory) es un tipo de 
+            red neuronal recurrente (RNN) especializada en trabajar 
+            con datos secuenciales, como series de tiempo, texto y 
+            datos de secuencia en general. A diferencia de las RNN 
+            tradicionales, que pueden sufrir de desvanecimiento o 
+            explosión del gradiente en entrenamientos a largo plazo, 
+            las capas LSTM están diseñadas para abordar estos problemas 
+            y capturar dependencias a largo plazo en los datos de entrada.
+            """)
 # Apartado para enviar un dato
 dato = st.text_input("Ingrese un dato:")
 enviar = st.button("Enviar")
